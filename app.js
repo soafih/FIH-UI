728063a634121f20c97aa2add1d8cbd90bd19c88
Shadab Hasan <shadab.hasan06@gmail.com>
2016-08-30 22:50:34 -0700

    MongoDb connection string changes

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var security = require('./routes/security');
 
 // Initialize Monk for establishing connection with MongoDB
 var monk = require('monk');
-//var db =  monk('mongodb://5eb31b82-3347-481f-a7f5-f9759fb2583a:22669d9e-531d-4176-bd5c-7844e1add561@10.135.4.49:15001/db');
 var db = monk(process.env.MONGODB_CONN_STR);
 console.log("MongoDB URL: "+process.env.MONGODB_CONN_STR);
 var app = express();
