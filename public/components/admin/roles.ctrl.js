be7329c11ff89ff10765345c7c03d0cc1cb1ada3
Shadab Hasan <shadab.hasan06@gmail.com>
2016-09-26 01:44:19 -0700

    Added roles search functionality

--- public/components/admin/roles.ctrl.js
+++ public/components/admin/roles.ctrl.js
@@ -1,35 +1,25 @@
 angular.module('fihApp').controller('RolesCtrl', function ($scope, $resource, $location, roleList, NgTableParams, $filter, prompt) {
     $scope.pageHeader = "Roles";
 
-    $scope.superUserOption = ["Select", "Yes", "No"];
-    $scope.selectedSuperUser = "Select";
-    $scope.rolesModel = [];
-    $scope.rolesData = [];
-    $scope.orgModel = [];
-    $scope.orgData = [];
     $scope.spaceModel = [];
     $scope.spaceData = [];
-
-    $scope.rolesCustomTexts = { buttonDefaultText: 'Select Roles' };
-    $scope.orgCustomTexts = { buttonDefaultText: 'Select Organizations' };
     $scope.spaceCustomTexts = { buttonDefaultText: 'Select Spaces' };
 
     $scope.selectSettings = {
         smartButtonMaxItems: 3
     };
-    $scope.users = roleList.data;
-    console.log("User Data: "+JSON.stringify($scope.users));
-    $scope.userTable = new NgTableParams(
+    $scope.roles = roleList.data;
+    $scope.roleTable = new NgTableParams(
         {
             page: 1,
-            count: 5
+            count: 8
         },
         {
             counts: [],
             paginationMaxBlocks: 13,
             paginationMinBlocks: 2,
             total: roleList.data.length,
-            dataset: $scope.users,
+            dataset: $scope.roles,
         }
     );
     $scope.checkboxes = { 'checked': false, items: {} };
@@ -65,28 +55,28 @@ angular.module('fihApp').controller('RolesCtrl', function ($scope, $resource, $l
         $location.path('/role-register');
     };
 
-    $scope.updateUser = function(){
-        var roleList = $scope.checkboxes.items;
-        console.log("All users items: "+JSON.stringify($scope.checkboxes.items));
+    $scope.updateRole = function(){
+        var selectedRoles = $scope.checkboxes.items;
+        console.log("Roles table items: "+JSON.stringify($scope.checkboxes.items));
         if($scope.countChecked === 1){
-            var selectedUserId = ""; 
-            for (var key in roleList) {
-                if (roleList.hasOwnProperty(key)) {
-                    if(roleList[key]){
-                        selectedUserId = key;
+            var selectedRoleId = ""; 
+            for (var key in selectedRoles) {
+                if (selectedRoles.hasOwnProperty(key)) {
+                    if(selectedRoles[key]){
+                        selectedRoleId = key;
                         break;
                     }
                 }
             }
-            console.log("Updating user: "+selectedUserId);
-            $location.path('/user-update/'+selectedUserId);
+            console.log("Updating role: "+selectedRoleId);
+            $location.path('/role-update/'+selectedRoleId);
         }
         else{
-            $scope.openPromptFailure("Please select exactly one user to update.!");
+            $scope.openPromptFailure("Please select exactly one role to update.!");
         }
     };
 
-    $scope.deleteUser = function(){
+    $scope.deleteRoles = function(){
         var roleList = $scope.checkboxes.items;
         console.log("All users items: "+JSON.stringify($scope.checkboxes.items));
         if($scope.countChecked !== 1){
@@ -151,44 +141,22 @@ angular.module('fihApp').controller('RolesCtrl', function ($scope, $resource, $l
         });
     };
     
-    $scope.searchUsers = function(){
+    $scope.searchRoles = function(){
         console.log("Applying filter..");
         
-        $scope.users = $filter('filter')(roleList.data, {username:$scope.txtUserName, email: $scope.txtEmail});
+        $scope.roles = $filter('filter')(roleList.data, {name:$scope.txtRoleName, descr: $scope.txtDescription});
         
-        if($scope.selectedSuperUser !== 'Select'){
-            var isSuperUser = false;
-            if($scope.selectedSuperUser === 'Yes')
-                isSuperUser = true;
-            $scope.users = $filter('filter')(roleList.data, {username:$scope.txtUserName, email: $scope.txtEmail, superuser:isSuperUser});
-        }
-        
-        if($scope.rolesModel.length > 0){
-            var selectedRoles = [];
-            for(var i=0; i<$scope.rolesModel.length; i++){
-                selectedRoles.push($scope.rolesModel[i].id);
-            }
-            $scope.users = $filter('roleFilter')($scope.users, selectedRoles);
-        }
-        if($scope.orgModel.length > 0){
-            console.log("Selected Org: "+JSON.stringify($scope.orgModel));
-            var selectedOrgs = [];
-            for(var i=0; i<$scope.orgModel.length;i++){
-                selectedOrgs.push($scope.orgModel[i].id);
-            }
-            $scope.users = $filter('orgFilter')($scope.users, selectedOrgs);
-        }
-        if($scope.spaceModel.length > 0){
+        /*if($scope.spaceModel.length > 0){
             var selectedSpace = [];
             for(var i=0; i<$scope.spaceModel.length;i++){
                 selectedSpace.push($scope.spaceModel[i].id);
             }
-            $scope.users = $filter('spaceFilter')($scope.users, selectedSpace);
-        }
-        $scope.userTable.total($scope.users.length);
-        $scope.userTable.settings().dataset = $scope.users;
-        $scope.userTable.reload();
-        console.log("Filtered Users: "+JSON.stringify($scope.users));
+            $scope.roles = $filter('spaceFilter')($scope.users, selectedSpace);
+        }*/
+        $scope.roleTable.total($scope.roles.length);
+        $scope.roleTable.settings().dataset = $scope.roles;
+        $scope.roleTable.reload();
+        console.log("Filtered Roles: "+JSON.stringify($scope.roles));
         
     };
     
@@ -206,45 +174,6 @@ angular.module('fihApp').controller('RolesCtrl', function ($scope, $resource, $l
         $scope.userTable.reload();
     };
 
-    var init = function () {
-        var SecurityAPI = $resource('/fih/users/roles');
-        SecurityAPI.query(function (roles) {
-            if (roles) {
-                $scope.roles = roles;
-                for (var i = 0; i < roles.length; i++) {
-                    if(roles[i].name !== "fih_admin"){
-                        $scope.rolesData.push({ id: roles[i].name, label: roles[i].name });
-                    }
-                }
-            }
-        });
-
-        var OrgAPI = $resource('/fih/stackatoapis/orgs');
-        OrgAPI.query(function (orgs) {
-            console.log("Fetched orgs: " + JSON.stringify(orgs));
-            if (orgs) {
-                $scope.orgs = orgs;
-                for (var i = 0; i < orgs.length; i++) {
-                    $scope.orgData.push({ id: orgs[i].name, label: orgs[i].name });
-                }
-            }
-        });
-
-        var SpaceAPI = $resource('/fih/stackatoapis/spaces');
-        SpaceAPI.query(function (spaces) {
-            console.log("Fetched orgs: " + JSON.stringify(spaces));
-            if (spaces) {
-                $scope.spaces = spaces;
-                for (var i = 0; i < spaces.length; i++) {
-                    $scope.spaceData.push({ id: spaces[i].name, label: spaces[i].name });
-                }
-            }
-        });
-
-    };
-
-    init();
-    
     $scope.openPromptFailure = function(message){
         prompt({
             title: 'Alert!',
@@ -279,36 +208,6 @@ angular.module('fihApp').controller('RolesCtrl', function ($scope, $resource, $l
         }).then(function(){
             //he hit ok and not can,
         });
-    };
-});
-
-fihApp.filter('roleFilter', function () {
-    return function (inputs, filterValues) {
-        console.log("Filter Input: "+JSON.stringify(inputs));
-        console.log("Filter Values: "+JSON.stringify(filterValues));
-        var output = [];
-        angular.forEach(inputs, function (input) {
-            angular.forEach(input.roles, function (role) {
-                if (filterValues.indexOf(role) !== -1){
-                    output.push(input);
-                }
-            });
-        });
-        return removeArrayDuplicate(output);
-    };
-});
-
-fihApp.filter('orgFilter', function () {
-    return function (inputs, filterValues) {
-        var output = [];
-        angular.forEach(inputs, function (input) {
-            angular.forEach(input.orgs, function (org) {
-                if (filterValues.indexOf(org.name) !== -1){
-                    output.push(input);
-                }
-            });
-        });
-        return removeArrayDuplicate(output);
     };
 });
 
